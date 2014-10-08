d4de3a18c166c5652434cec3c1d867b4d58efd0c
Rony J. Letona <zronyj@gmail.com>
2014-10-08 03:45:32 -0600

    Comenzando con la última parte del día 5 - Funciones TODO: - Nuestros paquetes - Mate avanzada - Química

--- Data/especiacion.py
+++ Data/especiacion.py
@@ -1,18 +1,20 @@
 K = [7.5E-3, 6.2E-8, 4.8E-13]
-f = open("diagrama.csv","w")
-for i in xrange(0,141,5):
-    ctrl = [0]*4
-    for j in xrange(len(K) + 1):
-        if j > 1:
-            ctrl[j] = K[j-1]*ctrl[j-1]/(K[j-1] + 10**(i*-0.1))
-            ctrl[j-1] -= ctrl[j]
-        elif j == 1:
-            ctrl[j] = K[j-1]/(K[j-1] + 10**(i*-0.1))
-            ctrl[j-1] -= ctrl[j]
-        else:
-            ctrl[j] = 1
-    result = str(i*0.1)
-    for k in ctrl:
-        result += " " + str(k)
-    f.write(result + "\n")
-f.close()
+def especiacion(*args):
+    f = open("diagrama.csv","w")
+    for i in xrange(0,141,1):
+        spec = len(args) + 1
+        ctrl = [0]*spec
+        for j in xrange(spec):
+            if j > 1:
+                ctrl[j] = args[j-1]*ctrl[j-1]/(args[j-1] + 10**(i*-0.1))
+                ctrl[j-1] -= ctrl[j]
+            elif j == 1:
+                ctrl[j] = args[j-1]/(args[j-1] + 10**(i*-0.1))
+                ctrl[j-1] -= ctrl[j]
+            else:
+                ctrl[j] = 1
+        result = str(i*0.1)
+        for k in ctrl:
+            result += " " + str(k)
+        f.write(result + "\n")
+    f.close()
