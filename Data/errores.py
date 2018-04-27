b34a06faab396d168475f566793d8f779dbe5b18
Rony J. Letona <zronyj@gmail.com>
2018-04-27 03:55:09 -0600

    Actualizando errores.py para funcionar con Python 3

--- Data/errores.py
+++ Data/errores.py
@@ -1,8 +1,8 @@
 # Ejemplo de error de sintaxis
-print "Error de ". "sintaxis."
+print ("Error de ". "sintaxis.")
 
 # Ejemplo de error de nombre
-print mi_variable
+print (mi_variable)
 
 # Ejemplo de error de division entre 0
 algun_numero = 15
@@ -22,7 +22,7 @@ mi_numero = float("esto es texto")
 
 # Ejemplo de error de indice
 lista_c = [5, 4, 3]
-print lista_c[6]
+print(lista_c[6])
 
 # Ejemplo de error de entrada y salida de datos (io)
 documento = open("algun_documento.txt", "r")
